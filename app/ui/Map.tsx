bf13615a79ee39be694280d27024c4e43895f301
fayekelmith <faye.12222883@lpu.in>
2023-12-07 14:06:06 +0530

    to build interface

---
+++ app/ui/Map.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+const LPUMap = () => {
+  return (
+    <div className=" mx-auto">
+      <Image src="/images/map.jpg" alt="map" width={800} height={800} />
+    </div>
+  );
+};
+
+export default LPUMap;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        blank: "#f5f7fa",
+        grey: "#e5ebf3",
+        noire: "#121212",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
