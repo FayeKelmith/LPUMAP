a200aabd8db29859a107d4e54fcb332b951b4177
fayekelmith <faye.12222883@lpu.in>
2023-12-08 23:42:59 +0530

    map set up and ready for interaction

--- app/ui/Map.tsx
+++ app/ui/Map.tsx
@@ -16,10 +16,18 @@ export function ChangeView({ coords }: any) {
 }
 
 const LPUMap = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true); // après le premier rendu, setIsMounted sera défini sur true
+  }, []);
   const [geoData, setGeoData] = useState({
     lat: 31.2524269616594,
     lng: 75.70253044336381,
   });
+  if (!isMounted) {
+    return null; // si le composant n'est pas monté, ne rien rendre
+  }
 
   const center = [geoData.lat, geoData.lng];
 
@@ -29,9 +37,16 @@ const LPUMap = () => {
       zoom={13}
       style={{ height: "60vh" }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer
+        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
+      />
+
       {geoData.lat && geoData.lng && (
-        <Marker position={[geoData.lat, geoData.lng]} alt="marker" />
+        <Marker
+          position={[geoData.lat, geoData.lng]}
+          alt="marker"
+          draggable={true}
+        />
       )}
       <ChangeView coords={center} />
     </MapContainer>
